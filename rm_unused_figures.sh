e4c44684414fb3dd270f8dea7c463a787da88868
Maël Valais <mael.valais@gmail.com>
2016-07-14 16:14:43 +0200

    enhanced my rm_unused_figures script

--- rm_unused_figures.sh
+++ rm_unused_figures.sh
@@ -1,40 +1,76 @@
 #! /bin/bash
 #
-# rm_unused_figures.sh
+# rm_mvpath_figures.sh
 # Copyright (C) 2016 Maël Valais <mael.valais@gmail.com>
 #
 # Use process substitution
 # while do
 #    ...
 # done < <(find samples/ )
-unused=_figures
+mvpath=_figures
 imgused=`./included_figures.sh`
 
-if [ $# -eq 0 ]; then
-    echo "dry-run mode"
-    mv=false
-elif [ $1 == "-f" ]; then
-    echo "move mode"
-    mv=true
+mv=false
+stays_display=true
+moves_display=true
+one_display=false
+
+while [ $# -ne 0 ]; do
+    if [ $1 == "-f" ]; then
+        mv=true
+    fi
+    if [ $1 == "-m" ]; then
+        moves_display=false
+    fi
+    if [ $1 == "-s" ]; then
+        stays_display=false
+    fi
+    shift
+done
+if [ $moves_display = false -o $stays_display = false ]; then
+    one_display=true
 fi
+
+if [ $mv = false ]; then
+    if [ $one_display = false ]; then echo "dry-run mode"; fi
+else
+    echo "move mode: are you sure? y/n"
+    read c
+    if [ $c != "y" ]; then
+        exit
+    fi
+fi
+
 while read f; do
     file=`echo $f | cut -d ":" -f1 | sed "s/^\.\///g"`
     found=false
     while IFS= read -r used && [ $found = false ]; do
         if [ "$file" == "$used" ]; then
             found=true
-            echo Stays: "$file"
+            output=""
+            if [ $one_display = false ]; then
+                output="Stays: "
+            fi
+            if [ $moves_display = true ]; then
+                echo "${output}${file}"
+            fi
         fi
     done < <( printf '%s\n' "$imgused" )
     if [ $found = false ]; then
-        echo Moves: "$file"
+        output=""
+        if [ $one_display = false ]; then
+            output="Moves: "
+        fi
+        if [ $stays_display = true ]; then
+            echo "${output}${file}"
+        fi
         if [ $mv = true ]; then
-            if ! [ -d "$unused" ]; then 
-                mkdir "$unused"; 
+            if ! [ -d "$mvpath" ]; then 
+                mkdir "$mvpath"; 
             fi
-            cp --parents "$file" "${unused}"
+            cp --parents "$file" "${mvpath}"
             rm "$file"
         fi
     fi
-done < <(find . -type f ! -path "*_figures*" -exec file {} \; | grep "image data")
+done < <(find . -type f ! -path "*${mvpath}*" -regex '.*\.\(png\|jpg\|pdf\|gif\)' -exec echo {} \;)
 
